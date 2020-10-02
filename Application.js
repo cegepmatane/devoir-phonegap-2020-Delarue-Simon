0e544ad9bce462c6913972eb40e4e156dd182c12
FuZzyy14 <simon.delarue2@gmail.com>
2020-10-02 15:27:09 +0200

    commit initial

---
+++ Application.js
@@ -0,0 +1,17 @@
+class Application{
+    constructor(window, veloDAO, vueListeVelo, vueAjouterVelo){
+
+
+    }
+
+
+
+}
+
+
+
+new Application(window, new VeloDAO(), new VueListeVelo(), new VueAjouterVelo())
+{
+
+
+}
